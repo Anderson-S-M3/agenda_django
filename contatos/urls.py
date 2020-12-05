bcf29b1b6681827a57aef005b3107c1a3b0c2589
Anderson-S-M3 <65872811+Anderson-S-M3@users.noreply.github.com>
2020-12-05 16:12:45 -0300

    Upload Agenda (#1)
    Update Agenda

---
+++ contatos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index_contatos'),
+    path('busca/', views.busca, name='busca'),
+    path('<int:contato_id>', views.detalhe_contato, name='detalhe_contato')
+]
